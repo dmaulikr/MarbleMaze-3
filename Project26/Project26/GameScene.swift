651b0a8df9d5c3a7a86bc28a7c1337eebd786c43
Jeff Eng <jeff@jeffeng.me>
2016-09-08 17:18:30 -0700

    added the loadLevel method that loads the level text file from disk

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
- 
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -19,5 +19,30 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
 
+    }
+    
+    func loadLevel() {
+        // get the file path for the level from NSBundle
+        if let levelPath = NSBundle.mainBundle().pathForResource("level1", ofType: "txt") {
+            if let levelString = try? String(contentsOfFile: levelPath, usedEncoding: nil) {
+                let lines = levelString.componentsSeparatedByString("\n")
+                
+                for (row, line) in lines.reverse().enumerate() {
+                    for (column, letter) in line.characters.enumerate() {
+                        let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                        
+                        if letter == "x" {
+                            // load wall
+                        } else if letter == "v" {
+                            // load vortex
+                        } else if letter == "s" {
+                            // load star
+                        } else if letter == "f" {
+                            // load finish
+                        }
+                    }
+                }
+            }
+        }
     }
 }
